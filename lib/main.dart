58ec278b2203e6fae42a50e3bcb8135b060f5729
Murshi-d <murshidkk2002@gmail.com>
2025-10-16 23:20:36 +0530

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,7 @@ import 'package:device_preview/device_preview.dart';
 
 void main() {
   runApp(
-    DevicePreview(
-      enabled: true,
-      builder: (context) => const Main(),
-    ),
+    const Main(),
   );
 }
 
